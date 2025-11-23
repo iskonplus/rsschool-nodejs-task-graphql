5903b843751effdc837cd84718ae20c748b3e910
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-11-23 19:32:32 +0100

    feat: add user(id) query

--- src/routes/graphql/schema.ts
+++ src/routes/graphql/schema.ts
@@ -1,6 +1,7 @@
-import { GraphQLSchema, GraphQLObjectType, GraphQLNonNull, GraphQLList } from 'graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLNonNull, GraphQLList, GraphQLError } from 'graphql';
 import { PrismaClient } from '@prisma/client';
 import { UserType, ProfileType, PostType, MemberTypeType } from './types/index.js';
+import { UUIDScalar } from './scalars.js';
 
 type GqlContext = {
   prisma: PrismaClient;
@@ -9,14 +10,39 @@ type GqlContext = {
 const RootQueryType = new GraphQLObjectType<GqlContext>({
   name: 'RootQueryType',
   fields: () => ({
-    // get user
+    // get users
     users: {
       type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
       async resolve(_src, _args, context) {
         return context.prisma.user.findMany();
       },
     },
-  }),
+
+    // get users by id
+    user: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDScalar) },
+      },
+      async resolve(
+        _source: unknown,
+        args: { id: string },
+        context: GqlContext,
+      ) {
+        const user = await context.prisma.user.findUnique({
+          where: { id: args.id },
+        });
+
+        if (!user) {
+          throw new GraphQLError('User not found');
+        }
+
+        return user;
+      }
+    },
+
+
+  })
 });
 
 const Mutations = new GraphQLObjectType({
