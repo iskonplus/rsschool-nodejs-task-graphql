0970daef370a633a5fde16a66be79aa3b262fe6f
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-11-23 16:05:09 +0100

    feat: add GraphQL types (User, Profile, Post, MemberType)

---
+++ src/routes/graphql/types/User.ts
@@ -0,0 +1,30 @@
+import {
+    GraphQLObjectType,
+    GraphQLNonNull,
+    GraphQLFloat,
+    GraphQLString,
+    GraphQLList,
+} from 'graphql';
+import { UUIDScalar } from '../scalars.js';
+// import { ProfileType } from './Profile.js';
+// import { PostType } from './Post.js';
+// import { MemberTypeType } from './MemberType.js';
+import { PostType, ProfileType, MemberTypeType } from './types/index.js';
+
+export const UserType = new GraphQLObjectType({
+
+    name: 'User',
+    fields: () => ({
+        id: { type: new GraphQLNonNull(UUIDScalar) },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        balance: { type: new GraphQLNonNull(GraphQLFloat) },
+
+        profile: { type: ProfileType },
+        posts: { type: new GraphQLList(PostType) },
+        memberType: { type: MemberTypeType },
+
+        userSubscribedTo: { type: new GraphQLList(UserType) },
+        subscribedToUser: { type: new GraphQLList(UserType) },
+    }),
+
+});
---
+++ src/routes/graphql/types/index.ts
@@ -0,0 +1,4 @@
+export * from './User.js';
+export * from './Profile.js';
+export * from './Post.js';
+export * from './MemberType.js';
