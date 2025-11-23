e4172c8f6d12b7bce92f7209268b83bf3483252b
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-11-23 19:23:18 +0100

    feat: add users query to RootQueryType

--- src/routes/graphql/schema.ts
+++ src/routes/graphql/schema.ts
@@ -1,22 +1,30 @@
-import { GraphQLSchema, GraphQLObjectType } from 'graphql';
-import {
-  UserType,
-  ProfileType,
-  PostType,
-  MemberTypeType,
-} from './types/index.js';
+import { GraphQLSchema, GraphQLObjectType, GraphQLNonNull, GraphQLList } from 'graphql';
+import { PrismaClient } from '@prisma/client';
+import { UserType, ProfileType, PostType, MemberTypeType } from './types/index.js';
 
-const RootQueryType = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: () => ({}),
+type GqlContext = {
+  prisma: PrismaClient;
+};
+
+const RootQueryType = new GraphQLObjectType<GqlContext>({
+  name: 'RootQueryType',
+  fields: () => ({
+    // get user
+    users: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
+      async resolve(_src, _args, context) {
+        return context.prisma.user.findMany();
+      },
+    },
+  }),
 });
 
 const Mutations = new GraphQLObjectType({
-    name: 'Mutations',
-    fields: () => ({}),
+  name: 'Mutations',
+  fields: () => ({}),
 });
 
 export const schema = new GraphQLSchema({
-    query: RootQueryType,
-    mutation: Mutations,
+  query: RootQueryType,
+  mutation: Mutations,
 });
